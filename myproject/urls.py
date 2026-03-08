7a3e016515c1f79ea8267c19c0076caea4424580
ShivamZode <zodeshivam537@gmail.com>
2026-03-08 23:59:57 +0530

    Update urlpatterns to serve media files
    Added URL patterns for serving media files during development.

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.urls import path, include, re_path
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from board import views
 
 urlpatterns = [
@@ -25,6 +27,8 @@ urlpatterns = [
     
 ]
 
-# This crucial line allows Django to act as an image server during local development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
