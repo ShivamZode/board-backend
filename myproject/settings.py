8306977ca4ee2b43712a21dee22b399d36732e12
ShivamZode <zodeshivam537@gmail.com>
2026-03-08 08:27:59 +0530

    Update DATABASES configuration for deployment
    Modify database settings to use environment variable for production.

--- myproject/settings.py
+++ myproject/settings.py
@@ -83,9 +83,17 @@ ASGI_APPLICATION = 'myproject.asgi.application' #new5
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(default="postgresql://neondb_owner:YOUR_PASSWORD@ep-cool-sun...neon.tech/neondb?sslmode=require")
-}
+if 'DATABASE_URL' in os.environ: #new15(for deploy)
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ['DATABASE_URL'], conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
